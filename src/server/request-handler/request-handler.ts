0ebc4ef2a3dba2b63e5d0de453c3d55f1578e5aa
wessberg <frederikwessberg@gmail.com>
2019-01-18 12:41:26 +0100

    fix(server): fixes issue where OPTIONS requests would return METHOD_NOT_ALLOWED

--- src/server/request-handler/request-handler.ts
+++ src/server/request-handler/request-handler.ts
@@ -3,6 +3,7 @@ import {Response} from "../i-response";
 import {printRequest} from "../../util/request-util/request-util";
 import {RequestHandlerOptions} from "./request-handler-options";
 import {constants} from "http2";
+import {METHOD_NOT_ALLOWED, NOT_FOUND, OK} from "http-status-codes";
 import {RegisteredControllers} from "../../controller/controller/registered-controllers";
 import {IStaticController} from "../../controller/static/i-static-controller";
 
@@ -27,10 +28,14 @@ export class RequestHandler implements IRequestHandler {
 		switch (options.request.method) {
 			case "GET":
 				return await this.handleGetRequest(options);
+			case "OPTIONS":
+				return await this.handleOptionsRequest(options);
 			default:
 				return {
 					body: `Only GET requests are supported`,
-					statusCode: constants.HTTP_STATUS_METHOD_NOT_ALLOWED,
+					statusCode: options.request.http2
+						? constants.HTTP_STATUS_METHOD_NOT_ALLOWED
+						: METHOD_NOT_ALLOWED,
 					contentType: "text/plain"
 				};
 		}
@@ -53,5 +58,31 @@ export class RequestHandler implements IRequestHandler {
 
 		// Fall back to the index route
 		return await this.staticController.onIndexRequested(options.request);
+	}
+
+	/**
+	 * Handles the given OPTIONS request
+	 * @param {RequestHandlerOptions} options
+	 * @returns {Promise<Response>}
+	 */
+	private async handleOptionsRequest (options: RequestHandlerOptions): Promise<Response> {
+
+		// Find the first matched controller
+		for (const controller of this.controllers) {
+			const match = controller.match(options.request);
+			if (match != null) {
+				return {
+					statusCode: options.request.http2
+						? constants.HTTP_STATUS_OK
+						: OK
+				};
+			}
+		}
+
+		return {
+			statusCode: options.request.http2
+				? constants.HTTP_STATUS_NOT_FOUND
+				: NOT_FOUND
+		};
 	}
 }

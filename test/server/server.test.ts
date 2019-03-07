ce4a91f047caee87c50b3e8ffb7936821bd09557
wessberg <frederikwessberg@gmail.com>
2019-03-07 14:27:39 +0100

    updated a test

--- test/server/server.test.ts
+++ test/server/server.test.ts
@@ -71,7 +71,7 @@ test("Will generate correct polyfills for IE11", async t => {
 		port: config.port,
 		path: `${
 			constant.endpoint.polyfill
-		}?features=web-components,es,class-list,system|variant=system,custom-event,url,fetch,object-fit,intersection-observer,animation,regenerator-runtime,requestanimationframe,requestidlecallback,resize-observer,pointer-event,dom.collections.iterable,scroll-behavior,zone|error,esnext.reflect`,
+		}?features=web-components,es,class-list,system|variant=system,custom-event,url,fetch,object-fit,intersection-observer,animation,regenerator-runtime,requestanimationframe,requestidlecallback,resize-observer,pointer-event,dom.collections.iterable,scroll-behavior,zone|error,esnext.reflect,intl|force|locale=en~da`,
 		acceptEncoding: undefined
 	});
 

6c478d88f80a58c0c9d77790578a344c5a23f49e
wessberg <frederikwessberg@gmail.com>
2020-10-14 01:09:19 +0200

    chore: write new deploy script

--- deploy.js
+++ deploy.js
@@ -59,31 +59,39 @@ server {
 	// Write the package.json file to desk temporarily
 	writeFileSync(PACKAGE_JSON_LOCAL_FILE_NAME, generatePackageJson());
 
-	// Connect to the hots machine via SSH
+	// Connect to the host machine via SSH
+	console.log(`Connecting to host machine via SSH...`);
 	const ssh = await new NodeSSH().connect({
 		host: DEPLOY_HOST,
 		username: DEPLOY_USER_NAME,
 		privateKey: SSH_KEY_LOCAL_FILE_NAME
 	});
+	console.log(`Successfully connected to host machine`);
 
 	// Get the last deployment data (if any such data exists)
+	console.log(`Checks if nginx config needs to be updated...`);
 	await ssh.getFile(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME, LAST_DEPLOYMENT_DATA_REMOTE_FILE_NAME);
 	const lastDeploymentData = existsSync(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME) ? JSON.parse(readFileSync(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME)) : undefined;
 
 	// If we have deployed in the past, check if the nginx config needs to be updated (for example if the ports or domain names changed)
-	if (lastDeploymentData != null) {
-		const needsNginxUpdate = lastDeploymentData.DEPLOY_PORT !== DEPLOY_PORT || lastDeploymentData.DEPLOY_DOMAIN_NAMES !== DEPLOY_DOMAIN_NAMES;
+	const needsNginxUpdate = lastDeploymentData == null || lastDeploymentData.DEPLOY_PORT !== DEPLOY_PORT || lastDeploymentData.DEPLOY_DOMAIN_NAMES !== DEPLOY_DOMAIN_NAMES;
 
-		if (needsNginxUpdate) {
-			// Write the nginx config to desk temporarily
-			writeFileSync(NGINX_CONFIG_LOCAL_FILE_NAME, generateNginxConfig());
+	if (needsNginxUpdate) {
+		console.log(`Nginx config needs update`);
+		// Write the nginx config to desk temporarily
+		writeFileSync(NGINX_CONFIG_LOCAL_FILE_NAME, generateNginxConfig());
 
-			// Update nginx config
-			await ssh.putFile(NGINX_CONFIG_LOCAL_FILE_NAME, NGINX_CONFIG_REMOTE_FILE_NAME);
+		// Update nginx config
+		console.log(`Updating Nginx config...`);
+		await ssh.putFile(NGINX_CONFIG_LOCAL_FILE_NAME, NGINX_CONFIG_REMOTE_FILE_NAME);
+		console.log(`Nginx config successfully updated`);
 
-			// Reload nginx
-			await ssh.execCommand(`sudo systemctl reload nginx`);
-		}
+		// Reload nginx
+		console.log(`Reloading nginx...`);
+		await ssh.execCommand(`sudo systemctl reload nginx`);
+		console.log(`Nginx successfully reloaded`);
+	} else {
+		console.log(`Nginx config is up to date`);
 	}
 
 	// Now, update the deployment data
@@ -98,19 +106,25 @@ server {
 			"  "
 		)
 	);
+	console.log(`Updating cached deployment stats`);
 	await ssh.putFile(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME, LAST_DEPLOYMENT_DATA_REMOTE_FILE_NAME);
+	console.log(`Successfully updated cached deployment stats`);
 
 	// Clean up the remote root
+	console.log("Cleaning up remote root");
 	await ssh.execCommand(`sudo rm -rf ${REMOTE_ROOT}`);
 	await ssh.execCommand(`sudo mkdir ${REMOTE_ROOT}`);
 
 	// Copy over the package.json file
+	console.log(`Creating ${PACKAGE_JSON_REMOTE_FILE_NAME}`);
 	await ssh.putFile(PACKAGE_JSON_LOCAL_FILE_NAME, PACKAGE_JSON_REMOTE_FILE_NAME);
 
 	// Copy over the built dist folder
+	console.log(`Creating ${DIST_REMOTE_FOLDER}`);
 	await ssh.putFile(DIST_LOCAL_FOLDER, DIST_REMOTE_FOLDER);
 
 	// Install
+	console.log(`Installing`);
 	await ssh.execCommand(`npm ci`);
 
 	const envVariables = Object.entries(process.env)
@@ -118,7 +132,9 @@ server {
 		.join(" ");
 
 	// Run
+	console.log(`Running`);
 	await ssh.execCommand(`${envVariables} pm2 reload all`);
+	console.log("`Done!");
 })()
 	.catch(() => process.exit(1))
 	.then(() => process.exit(0));

9a4684a6234b0ac35c868c14142ee11bc946b1c9
wessberg <frederikwessberg@gmail.com>
2020-10-14 02:46:23 +0200

    chore: write new deploy script

--- deploy.js
+++ deploy.js
@@ -1,22 +1,29 @@
-const {NodeSSH} = require("node-ssh");
-const pkg = require("./package.json");
-const {join} = require("path");
-const {writeFileSync, readFileSync, existsSync} = require("fs");
+(async () => {
+	const {NodeSSH} = require("node-ssh");
+	const pkg = require("./package.json");
+	const {join, dirname} = require("path");
+	const {writeFileSync, readFileSync, existsSync, mkdirSync, copyFileSync} = require("fs");
 
-const generatePackageJson = () =>
-	JSON.stringify({
-		...pkg,
-		devDependencies: {},
-		scripts: {
-			start: "pm2 start dist/index.js"
-		},
-		dependencies: {
-			...pkg.dependencies,
-			pm2: "latest"
-		}
-	});
+	const {DEPLOY_HOST, DEPLOY_USER_NAME, DEPLOY_KEY, DEPLOY_KEY_LOCATION, DEPLOY_DOMAIN_NAMES, HOST, PORT} = process.env;
 
-const generateNginxConfig = () => `\
+	const generatePackageJson = () =>
+		JSON.stringify(
+			{
+				...pkg,
+				devDependencies: {},
+				scripts: {
+					start: "node dist/index.js"
+				},
+				dependencies: {
+					...pkg.dependencies,
+					pm2: "latest"
+				}
+			},
+			null,
+			"  "
+		);
+
+	const generateNginxConfig = () => `\
 server {
         listen 80 default_server;
         listen [::]:80 default_server;
@@ -28,7 +35,7 @@ server {
         server_name ${DEPLOY_DOMAIN_NAMES};
 
         location / {
-                proxy_pass http://localhost:${DEPLOY_PORT};
+                proxy_pass http://localhost:${PORT};
 								proxy_http_version 1.1;
 								proxy_set_header Upgrade $http_upgrade;
 								proxy_set_header Connection 'upgrade';
@@ -38,29 +45,36 @@ server {
 }
 `;
 
-(async () => {
-	const {DEPLOY_HOST, DEPLOY_USER_NAME, DEPLOY_KEY, DEPLOY_DOMAIN_NAMES, DEPLOY_PORT} = process.env;
-
+	const PREFERRED_NODE_VERSION = "14.x";
+	const APP_NAME = "polyfiller";
+	const LOCAL_WRITE_ROOT = "temp";
 	const REMOTE_ROOT = "/var/www/polyfiller";
 	const DIST_LOCAL_FOLDER = "dist";
 	const DIST_REMOTE_FOLDER = join(REMOTE_ROOT, "dist");
-	const PACKAGE_JSON_LOCAL_FILE_NAME = `_package.json`;
+	const PACKAGE_LOCK_LOCAL_FILE_NAME = join(LOCAL_WRITE_ROOT, `package-lock.json`);
+	const PACKAGE_LOCK_REMOTE_FILE_NAME = join(REMOTE_ROOT, `package-lock.json`);
+	const PACKAGE_JSON_LOCAL_FILE_NAME = join(LOCAL_WRITE_ROOT, `package.json`);
 	const PACKAGE_JSON_REMOTE_FILE_NAME = join(REMOTE_ROOT, `package.json`);
 
-	const SSH_KEY_LOCAL_FILE_NAME = `ssh-key`;
-	const NGINX_CONFIG_LOCAL_FILE_NAME = `nginx-config`;
+	const SSH_KEY_LOCAL_FILE_NAME = join(LOCAL_WRITE_ROOT, `ssh-key`);
+	const NGINX_CONFIG_LOCAL_FILE_NAME = join(LOCAL_WRITE_ROOT, `nginx-config`);
 	const NGINX_CONFIG_REMOTE_FILE_NAME = `/etc/nginx/sites-available/default`;
-	const LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME = `last-deployment-data.json`;
+	const LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME = join(LOCAL_WRITE_ROOT, `last-deployment-data.json`);
 	const LAST_DEPLOYMENT_DATA_REMOTE_FILE_NAME = `/var/www/last-deployment-data.json`;
 
 	// Write the key to desk temporarily
-	writeFileSync(SSH_KEY_LOCAL_FILE_NAME, DEPLOY_KEY);
+	mkdirSync(dirname(SSH_KEY_LOCAL_FILE_NAME), {recursive: true});
+	writeFileSync(SSH_KEY_LOCAL_FILE_NAME, DEPLOY_KEY ?? readFileSync(DEPLOY_KEY_LOCATION, "utf8"));
 
 	// Write the package.json file to desk temporarily
+	mkdirSync(dirname(PACKAGE_JSON_LOCAL_FILE_NAME), {recursive: true});
 	writeFileSync(PACKAGE_JSON_LOCAL_FILE_NAME, generatePackageJson());
 
+	// Copy the package-lock into the temp folder
+	copyFileSync("package-lock.json", PACKAGE_LOCK_LOCAL_FILE_NAME);
+
 	// Connect to the host machine via SSH
-	console.log(`Connecting to host machine via SSH...`);
+	console.log(`Connecting to host machine via SSH`);
 	const ssh = await new NodeSSH().connect({
 		host: DEPLOY_HOST,
 		username: DEPLOY_USER_NAME,
@@ -68,13 +82,65 @@ server {
 	});
 	console.log(`Successfully connected to host machine`);
 
+	console.log("Checking for make support");
+	const needsMake = (await ssh.execCommand("which make")).stdout === "";
+	const needsGPlusPlus = (await ssh.execCommand("which g++")).stdout === "";
+	if (needsMake || needsGPlusPlus) {
+		console.log("make is missing on the host machine. Installing");
+		await ssh.execCommand(`sudo apt-get install build-essential -y`);
+		console.log("Make successfully installed!");
+	} else {
+		console.log("Make is already supported");
+	}
+
+	console.log("Checking for Node.js support");
+	const needsNode = (await ssh.execCommand("which node")).stdout === "";
+	if (needsNode) {
+		console.log("Node.js is missing on the host machine. Installing");
+		await ssh.execCommand(`cd ~`);
+		await ssh.execCommand(`curl -sL https://deb.nodesource.com/setup_${PREFERRED_NODE_VERSION} -o nodesource_setup.sh`);
+		await ssh.execCommand(`sudo bash nodesource_setup.sh`);
+		await ssh.execCommand(`sudo apt install nodejs -y`);
+		await ssh.execCommand(`rm nodesource_setup.sh`);
+		console.log("Node.js successfully installed!");
+	} else {
+		console.log("Node.js is already supported");
+	}
+
+	console.log("Checking for Nginx support");
+	const needsNginx = (await ssh.execCommand("which nginx")).stdout === "";
+	if (needsNginx) {
+		console.log("Nginx is missing on the host machine. Installing");
+		await ssh.execCommand(`sudo apt-get update && sudo apt-get install nginx -y`);
+		await ssh.execCommand(`sudo ufw allow 'Nginx Full'`);
+		console.log("Nginx successfully installed!");
+	} else {
+		console.log("Nginx is already supported");
+	}
+
+	console.log("Checking for Certbot support");
+	const needsCertbot = (await ssh.execCommand("which certbot")).stdout === "";
+	if (needsCertbot) {
+		console.log("Certbot is missing on the host machine. Installing");
+		await ssh.execCommand(`sudo add-apt-repository ppa:certbot/certbot`);
+		await ssh.execCommand(`sudo apt-get update`);
+		await ssh.execCommand(`sudo apt install python-certbot-nginx -y`);
+		console.log("Certbot successfully installed!");
+	} else {
+		console.log("Certbot is already supported");
+	}
+
 	// Get the last deployment data (if any such data exists)
-	console.log(`Checks if nginx config needs to be updated...`);
-	await ssh.getFile(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME, LAST_DEPLOYMENT_DATA_REMOTE_FILE_NAME);
+	console.log(`Check if nginx config needs to be updated`);
+	try {
+		await ssh.getFile(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME, LAST_DEPLOYMENT_DATA_REMOTE_FILE_NAME, undefined);
+	} catch {
+		// The file doesn't exist
+	}
 	const lastDeploymentData = existsSync(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME) ? JSON.parse(readFileSync(LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME)) : undefined;
 
 	// If we have deployed in the past, check if the nginx config needs to be updated (for example if the ports or domain names changed)
-	const needsNginxUpdate = lastDeploymentData == null || lastDeploymentData.DEPLOY_PORT !== DEPLOY_PORT || lastDeploymentData.DEPLOY_DOMAIN_NAMES !== DEPLOY_DOMAIN_NAMES;
+	const needsNginxUpdate = lastDeploymentData == null || lastDeploymentData.PORT !== PORT || lastDeploymentData.DEPLOY_DOMAIN_NAMES !== DEPLOY_DOMAIN_NAMES;
 
 	if (needsNginxUpdate) {
 		console.log(`Nginx config needs update`);
@@ -82,12 +148,12 @@ server {
 		writeFileSync(NGINX_CONFIG_LOCAL_FILE_NAME, generateNginxConfig());
 
 		// Update nginx config
-		console.log(`Updating Nginx config...`);
+		console.log(`Updating Nginx config`);
 		await ssh.putFile(NGINX_CONFIG_LOCAL_FILE_NAME, NGINX_CONFIG_REMOTE_FILE_NAME);
 		console.log(`Nginx config successfully updated`);
 
 		// Reload nginx
-		console.log(`Reloading nginx...`);
+		console.log(`Reloading nginx`);
 		await ssh.execCommand(`sudo systemctl reload nginx`);
 		console.log(`Nginx successfully reloaded`);
 	} else {
@@ -99,7 +165,7 @@ server {
 		LAST_DEPLOYMENT_DATA_LOCAL_FILE_NAME,
 		JSON.stringify(
 			{
-				DEPLOY_PORT,
+				PORT,
 				DEPLOY_DOMAIN_NAMES
 			},
 			null,
@@ -112,29 +178,42 @@ server {
 
 	// Clean up the remote root
 	console.log("Cleaning up remote root");
-	await ssh.execCommand(`sudo rm -rf ${REMOTE_ROOT}`);
-	await ssh.execCommand(`sudo mkdir ${REMOTE_ROOT}`);
+	await ssh.execCommand(`sudo rm -rf ${DIST_REMOTE_FOLDER}`);
+	await ssh.execCommand(`mkdir -rf ${REMOTE_ROOT}`);
 
-	// Copy over the package.json file
-	console.log(`Creating ${PACKAGE_JSON_REMOTE_FILE_NAME}`);
-	await ssh.putFile(PACKAGE_JSON_LOCAL_FILE_NAME, PACKAGE_JSON_REMOTE_FILE_NAME);
+	// Copy over the package.json and package-lock.json files
+	console.log(`Creating ${PACKAGE_JSON_REMOTE_FILE_NAME} and ${PACKAGE_LOCK_REMOTE_FILE_NAME}`);
+	await ssh.putFiles([
+		{
+			local: PACKAGE_JSON_LOCAL_FILE_NAME,
+			remote: PACKAGE_JSON_REMOTE_FILE_NAME
+		},
+		{
+			local: PACKAGE_LOCK_LOCAL_FILE_NAME,
+			remote: PACKAGE_LOCK_REMOTE_FILE_NAME
+		}
+	]);
 
 	// Copy over the built dist folder
 	console.log(`Creating ${DIST_REMOTE_FOLDER}`);
-	await ssh.putFile(DIST_LOCAL_FOLDER, DIST_REMOTE_FOLDER);
+	await ssh.putDirectory(DIST_LOCAL_FOLDER, DIST_REMOTE_FOLDER);
 
 	// Install
 	console.log(`Installing`);
-	await ssh.execCommand(`npm ci`);
-
-	const envVariables = Object.entries(process.env)
-		.map(([key, value]) => `${key}=${value}`)
-		.join(" ");
+	await ssh.execCommand(`npm ci`, {cwd: REMOTE_ROOT});
 
 	// Run
 	console.log(`Running`);
-	await ssh.execCommand(`${envVariables} pm2 reload all`);
+	const pm2NeverRan = (await ssh.execCommand(`npx pm2 show ${APP_NAME}`, {cwd: REMOTE_ROOT})).stdout === "";
+	if (pm2NeverRan) {
+		await ssh.execCommand(`HOST=${HOST} PORT=${PORT} npx pm2 start npm --name "${APP_NAME}" -- start`, {cwd: REMOTE_ROOT});
+	} else {
+		await ssh.execCommand(`HOST=${HOST} PORT=${PORT} npx pm2 reload ${APP_NAME}`, {cwd: REMOTE_ROOT});
+	}
 	console.log("`Done!");
 })()
-	.catch(() => process.exit(1))
+	.catch(ex => {
+		console.error(ex);
+		process.exit(1);
+	})
 	.then(() => process.exit(0));

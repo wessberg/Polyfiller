5c65ab7341345f11288254b5560c6cb5b0f1df89
Frederik Wessberg <frederikwessberg@gmail.com>
2022-05-05 20:10:18 +0200

    fix: fix lint

--- src/util/type/ecma-version.ts
+++ src/util/type/ecma-version.ts
@@ -1,1 +1,1 @@
-export type EcmaVersion = "es3" | "es5" | "es2015" | "es2016" | "es2017" | "es2018" | "es2019" | "es2020";
+export type EcmaVersion = "es3" | "es5" | "es2015" | "es2016" | "es2017" | "es2018" | "es2019" | "es2020" | "es2021" | "es2022";
